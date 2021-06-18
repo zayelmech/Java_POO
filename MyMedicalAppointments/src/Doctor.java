044034136ee8de92127ce8bd90acfad55fcf6e48
Abdiel Carreño <56939611+zayelmech@users.noreply.github.com>
2021-06-18 17:09:07 -0500

    Creando elementos estaticos clase 8

--- MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppointments/src/Doctor.java
@@ -1,12 +1,16 @@
 public class Doctor {
-    int id;
+    static int id =0 ; //autoincrement
     String name;
     String speciality;
     Doctor(){
         System.out.println("Construyendo el Objeto Doctor");
+        id++;
     }
     //Comportamientos
     public void showName(){
         System.out.println(name);
+    }
+    public void showId(){
+        System.out.println("ID Doctor: "+id);
     }
 }
--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -1,3 +1,5 @@
+import static UI.UIMenu.*;
+
 public class Main {
     public static void main(String[] args){
 
@@ -5,6 +7,13 @@ public class Main {
         Doctor myDoctor = new Doctor();
         myDoctor.name="Alejandro Rodriguez";
         myDoctor.showName();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
 
+        Doctor myDoctorAnn = new Doctor();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+
+        showMenu();
     }
 }
