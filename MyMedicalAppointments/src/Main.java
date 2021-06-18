2976bf55f9672bd2bc2979dc12cb67370b40ed43
Abdiel Carreño <56939611+zayelmech@users.noreply.github.com>
2021-06-18 17:28:27 -0500

    Final: variables constantes y reto clase 9 y 10

--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -2,7 +2,7 @@ import static UI.UIMenu.*;
 
 public class Main {
     public static void main(String[] args){
-
+/*
         //Declarar y instanciar un objeto
         Doctor myDoctor = new Doctor();
         myDoctor.name="Alejandro Rodriguez";
@@ -13,7 +13,7 @@ public class Main {
         Doctor myDoctorAnn = new Doctor();
         myDoctor.showId();
         System.out.println(Doctor.id);
-
+*/
         showMenu();
     }
 }
--- MyMedicalAppointments/src/UI/UIMenu.java
+++ MyMedicalAppointments/src/UI/UIMenu.java
@@ -3,6 +3,8 @@ package UI;
 import java.util.Scanner;
 
 public class UIMenu {
+    public static final String[] MONTHS = {"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+
     public static void showMenu(){
         System.out.println("Welcome to My Appointments");
         System.out.println("Selecciona la opción deseada");
@@ -49,6 +51,9 @@ public class UIMenu {
             switch (response){
                 case 1:
                     System.out.println("::Book an appointment");
+                    for (int i = 1; i <4 ; i++) {
+                        System.out.println(i +"." + MONTHS[i-1]);
+                    }
                     break;
                 case 2:
                     System.out.println("::My appointments");
